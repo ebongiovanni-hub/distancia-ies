c75e2a9d150eeb4eaea65ac1a3842a8037854130
ebongiovanni-hub <ebongiovanni@ies21.edu.ar>
2026-08-21 19:49:22 -0300

    Create firma.js

---
+++ js/firma.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",()=>{
+  document.querySelectorAll(".footer-proyecto").forEach(footer=>{
+    footer.textContent="Modalidad Distancia - IES";
+  });
+});
