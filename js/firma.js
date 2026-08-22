5249f9c3cbec1fff3295a4e250599f1e73ca8949
ebongiovanni-hub <ebongiovanni@ies21.edu.ar>
2026-08-21 23:35:58 -0300

    Update firma.js

--- js/firma.js
+++ js/firma.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",()=>{
   document.querySelectorAll(".footer-proyecto").forEach(footer=>{
-    footer.innerHTML='<div class="footer-line"><span></span><i></i><span></span></div><div class="footer-title">IES · MODALIDAD DISTANCIA</div><div class="footer-sub">Innovación, acompañamiento y cercanía</div>';
+    footer.innerHTML='<div class="footer-line"><span></span><i></i><span></span></div><div class="footer-title">MODALIDAD DISTANCIA</div><div class="footer-sub">Innovación, acompañamiento y cercanía</div>';
   });
 });
